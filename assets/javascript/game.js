6fd26414257423db6961fc73aa11a04cadaa1e22
Trevor Satterwhite <trevorsatterwhite@Trevors-MacBook-Pro.local>
2017-05-11 22:55:24 -0400

    update

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+
+
+function wins()
+{
+	console.log(event.type);
+    var text = "";
+    var possible = "abcdefghijklmnopqrstuvwxyz";
+
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+}
