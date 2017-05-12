67c901f01e0d7ed4812a1ca07c24f089193d9ea0
Trevor Satterwhite <trevorsatterwhite@Trevors-MacBook-Pro.local>
2017-05-12 00:19:57 -0400

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,26 @@
+var options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z'];
+document.onkeyup = function(event) {
 
+    var userGuess = event.key;
+    alert(userGuess);
+    var computerGuess = options[Math.floor(Math.random() * options.length)];
+    alert(computerGuess);
+    if (options.includes(userGuess)) {
 
-function wins()
-{
-	console.log(event.type);
-    var text = "";
-    var possible = "abcdefghijklmnopqrstuvwxyz";
+        if (userGuess == computerGuess) {
+            alert("You Win!");
+        } else if (userGuess != computerGuess) {
+                alert('Computer Wins!');
+            }
+        
+    }
 
-    for( var i=0; i < 5; i++ )
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
+var lives = 4;
+if (lives < 1) {
+    lives -=1;
+    document.getElementById("lives").innerHTML = Main.lives;
 }
+
+
+}
+
